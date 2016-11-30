f7232a13444488b473c2daebb664ad804ca31ccc
piperjames <fireisaverywickedmaster@gmail.com>
2016-11-30 11:59:55 +0000

    ..........

--- scanner.py
+++ scanner.py
@@ -2,38 +2,48 @@
 
 from listhosts import gethosts
 from scanhosts import hostinfo
-from sys import argv
+import optparse
 import os
 
+def isIP(host):
+    if not "/" in host and not host.isalnum():
+        if len(host.split(".")) == 4:
+            return(True)
+        else:
+            return(False)
+
 def main():
-	if len(argv)==3:
-		iface = argv[1]
-		fast = argv[2]
-		hosts = gethosts(iface)
-		baseip, netips = hosts
-		options = (True,False)
-		for ip in netips:
-			if fast in options:
-				hostinfo(('.'.join((baseip), str(ip)),fast))
-		else:
-			hostinfo('.'.join((baseip, str(ip))))
-            
-	elif len(argv)==2:
-		iface = argv[1]
-		hosts = gethosts(iface)
-		baseip, netips = hosts
-		for ip in netips:
-			hostinfo('.'.join((baseip, str(ip))))
-	else:
-		iface = str(input('Enter Interface: '))
-		hosts = gethosts(iface)
-		baseip, netips = hosts
-		for ip in netips:
-			hostinfo('.'.join((baseip, str(ip))))
+    parser = optparse.OptionParser('usage%prog -h|-c <host|cidr> -f <True|False>')
+    parser.add_option('-H', dest='Host', type='string',\
+            help='specify target host')
+    parser.add_option('-c', dest='CIDR', type='string',\
+            help='specify cidr')
+    parser.add_option('-f', dest='Speed', type='string', default=False,\
+            help='specify scan level')
+    (options,args) = parser.parse_args()
+    host = options.Host
+    CIDR = options.CIDR
+    speed = options.Speed
 
+    if (host == None) and (CIDR == None):
+        print('[-] Specify a host or cidr to scan')
+        exit(0)
 
-if __name__=='__main__':
-	if os.geteuid() != 0:
-		print('This scripts needs to be run as root')
-	else:
-		main()
+    if host and not CIDR:
+        if isIP(host):
+            hostinfo(host, speed)
+        else:
+            print('[-] Specify a correct host')
+            exit(0)
+
+    elif CIDR and not host:
+        hosts = gethosts(CIDR)
+        baseip, netips = hosts
+        for ip in netips:
+            hostinfo('.'.join((baseip, str(ip))),speed)
+
+if __name__=="__main__":
+    if os.geteuid() != 0:
+        print('This script needs to be run as root')
+    else:
+        main()
