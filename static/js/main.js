21c344a5f1071220d92f774ab23c359bf97d0514
piperjames <fireisaverywickedmaster@gmail.com>
2017-06-06 13:34:59 +0000

    whew

--- static/js/main.js
+++ static/js/main.js
@@ -5,14 +5,7 @@ $(function(){
 	$(document).ajaxStop(function(){
 		$('.title img').hide();
 	});
-<<<<<<< HEAD
-	
-=======
-<<<<<<< HEAD
-	
-=======
->>>>>>> f1ce60a788156a189832a523ebf723823a1bac8a
->>>>>>> 22f366793deab89a708ca4f45c315f383d4b0a67
+
     $('.sbutton').on('click',function(){
 	    $.get('netlist',{
 		    inet: $('.inet').val()
@@ -20,30 +13,15 @@ $(function(){
 		    $('.iplist ul').html(data);
 	});
     });
-<<<<<<< HEAD
 	
 	$('.iplist ul').on('click','li.toscan',function(){
-=======
-<<<<<<< HEAD
-	
-	$('li.toscan').on('click',function(){
->>>>>>> 22f366793deab89a708ca4f45c315f383d4b0a67
 		$.get('gethostname',{
 			host: $(this).text()
 		}, function(data){
 			$('.hostname').text(data);
 		});
-<<<<<<< HEAD
 		$('.ipname').text($(this).text())
 		$('.ipscan').show();
 		$('.dashboard').hide();
 	});
-=======
-		$('.dashboard').hide();
-		$('.ipname').text($(this).text())
-		$('.ipscan').show();
-	});
-=======
->>>>>>> f1ce60a788156a189832a523ebf723823a1bac8a
->>>>>>> 22f366793deab89a708ca4f45c315f383d4b0a67
 });
